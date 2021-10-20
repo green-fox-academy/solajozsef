606d6a718451aa5042f9dd4400d302cc152442c2
Sola Jozsef <sola.jozsef@gmail.com>
2021-10-20 15:21:54 +0200

    uploading LogicalValues

--- week-02/day-2/src/com/greenfox/comments/Comments.java
+++ week-02/day-2/src/com/greenfox/comments/Comments.java
@@ -2,11 +2,17 @@ package com.greenfox.comments;
 
 public class Comments {
     public static void main(String[] args) {
-    // egy soros comment
+        // egy soros comment
 
 /*
  Több soros
  comment blokk
 */
-  }
+        int a = 10;
+        int b = a + --a;
+        System.out.println("b = " + b);
+     //   System.out.println( false || false);
+
+    }
 }
+
