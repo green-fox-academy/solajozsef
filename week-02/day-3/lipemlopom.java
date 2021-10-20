2e6fc11c9f3e39e0c63262060284e093412745a3
Sola Jozsef <sola.jozsef@gmail.com>
2021-10-20 08:27:25 +0200

    adding lipemlopom.java

---
+++ week-02/day-3/lipemlopom.java
@@ -0,0 +1,12 @@
+package com.greenfox.lipemlopom;
+
+public class LipemLopom {
+    public static void main(String[] args) {
+        // Írasd ki a "Lipem lopom a szőlőt" című mondóka sorait helyes sorrendben
+        //System.out.println("elaludt az öreg csősz.");
+        System.out.println("Lipem, lopom a szőlőt," + " elaludt az öreg csősz.");
+        // System.out.println("vaskalap a fejébe.");
+        System.out.println("Furkósbot a kezébe," + " vaskalap a fejébe.");
+    }
+}
+
