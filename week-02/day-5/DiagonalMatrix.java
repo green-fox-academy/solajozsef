5c33fee0b19d6bda6dcc05216df4c5256d28ac07
Sola Jozsef <sola.jozsef@gmail.com>
2021-10-22 16:52:03 +0200

    uploading 3 more files

---
+++ week-02/day-5/DiagonalMatrix.java
@@ -0,0 +1,41 @@
+package com.greenfox.arrays;
+
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        // - Készíts (dinamikusan*) egy kétdimenziós tömböt
+//   a következő mátrix-szal**. Ciklust (loop-ot) használj!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Írasd ki a konzolra ezt a kétdimenziós tömböt
+// * A tömb hossza egy változótól kell függjön
+// ** Nyugalom, egy mátrix pont olyan, mint egy tömb
+
+        int variab = 5;
+        int[][] array = new int[5][5];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == j) {
+                    array[i][j] = 1;
+                } else {
+                    array[i][j] = 0;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+
+
+    }
+}
